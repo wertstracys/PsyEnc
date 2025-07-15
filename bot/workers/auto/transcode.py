1b92f2f0972d48b673032db5274ecf450f434bfc
Tracy S Werts <wertstracys@gmail.com>
2025-07-14 20:06:42 -0400

    update transcode.py

--- bot/workers/auto/transcode.py
+++ bot/workers/auto/transcode.py
@@ -377,7 +377,7 @@ async def thing():
             mux_args = await another(mux_args, title, epi, sn, metadata_name, o_out)
             ffmpeg = (
                 'ffmpeg -i """{}""" '
-                f"{mux_args} -map 0:v:0 -map 0:a? -map 0:s? -map 0:t?"
+                f"{mux_args} -map 0:v:0 -map 0:a? -map 0:s?"
                 ' """{}""" -y'
             )
             _out = split_ext(out)[0] + " [Muxing]" + split_ext(out)[1]
